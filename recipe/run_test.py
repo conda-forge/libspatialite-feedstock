c3ca06b86be3c0c054e50af39216fe2f2010dfe9
ocefpaf <ocefpaf@gmail.com>
2016-11-14 14:09:43 -0300

    Some clean ups.

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,4 +1,3 @@
-
 import os
 import sys
 import ctypes
@@ -7,9 +6,11 @@ import platform
 if sys.platform == 'win32':
     libfreexl = ctypes.CDLL('spatialite.dll')
 elif sys.platform == 'darwin':
-    # LD_LIBRARY_PATH not set on OSX or Linux
+    # LD_LIBRARY_PATH not set on OS X or Linux.
     path = os.path.expandvars('$PREFIX/lib/libspatialite.dylib')
     libfreexl = ctypes.CDLL(path)
-else:
+elif sys.platform.startswith('linux'):
     path = os.path.expandvars('$PREFIX/lib/libspatialite.so')
     libfreexl = ctypes.CDLL(path)
+else:
+    raise Exception('Cannot recognize platform {!r}'.format(sys.platform))
