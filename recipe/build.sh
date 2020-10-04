b38a6f3f115ea735d506ea4157db8a8233456502
Xylar Asay-Davis <xylarstorm@gmail.com>
2020-10-04 14:54:26 +0200

    libspatialite 5.0.0
    Commend out patches and put back make check
    
    Added tests for main include and libs

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
 export LDFLAGS="-L${PREFIX}/lib ${LDFLAGS}"
-# The `ACCEPT_USE_OF_DEPRECATED_PROJ_API_H` is a temporary solution and won't work with proj4 7.
-export CFLAGS="-I${PREFIX}/include -DACCEPT_USE_OF_DEPRECATED_PROJ_API_H=1  ${CFLAGS}"
+export CFLAGS="-I${PREFIX}/include  ${CFLAGS}"
 
 # these files have hardcoded paths in them.  We don't need .la files anyway, so just remove it.
-if [ -f ${PREFIX}/${HOST}/lib/libstdc++.la ]; then
-    find ${PREFIX} -name "*.la" -print0 | xargs -0 rm
-fi
+# if [ -f ${PREFIX}/${HOST}/lib/libstdc++.la ]; then
+#    find ${PREFIX} -name "*.la" -print0 | xargs -0 rm
+# fi
 
 ./configure --prefix=${PREFIX} \
             --host=${HOST} \
             --build=${BUILD} \
-            --enable-static=no
+            --enable-static=no \
+            --enable-minizip=no
 
 make
-# Commented out due to failures:
-# FAIL: check_virtualtable2
-# FAIL: check_virtualtable
-# check_sql_stmt
-# make check
+make check
 make install
