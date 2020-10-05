691919c74901299a567ad470edcecb697469f1ad
Xylar Asay-Davis <xylarstorm@gmail.com>
2020-10-05 07:58:11 +0200

    Update testing to include a test for mod_spatialite
    Remove redundant testing in meta.yaml

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import ctypes
+import sqlite3
 
 if sys.platform == 'win32':
     path = os.path.join(sys.prefix, 'Library', 'bin', 'spatialite.dll')
@@ -14,3 +15,20 @@ elif sys.platform.startswith('linux'):
     libfreexl = ctypes.CDLL(path)
 else:
     raise Exception('Cannot recognize platform {!r}'.format(sys.platform))
+
+if sys.platform == 'win32':
+    path = os.path.join(sys.prefix, 'Library', 'bin', 'mod_spatialite.dll')
+elif sys.platform == 'darwin':
+    # LD_LIBRARY_PATH not set on OS X or Linux.
+    path = os.path.expandvars('$PREFIX/lib/mod_spatialite.dylib')
+elif sys.platform.startswith('linux'):
+    path = os.path.expandvars('$PREFIX/lib/mod_spatialite.so')
+else:
+    raise Exception('Cannot recognize platform {!r}'.format(sys.platform))
+
+# Test loading the resulting SQLite3 extension
+db = sqlite3.connect(':memory:')
+db.enable_load_extension(True)
+db.execute("SELECT load_extension(?)", [path])
+db.enable_load_extension(False)
+db.close()
