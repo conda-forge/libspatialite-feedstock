1f9010affc99021a6a485664cbb637071cd7e727
Xylar Asay-Davis <xylarstorm@gmail.com>
2020-10-05 09:11:06 +0200

    Add (and patch) win build of mod_spatialite

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,6 +2,12 @@
 nmake /f makefile.vc
 if errorlevel 1 exit 1
 
+nmake /f makefile_mod.vc
+if errorlevel 1 exit 1
+
 nmake /f makefile.vc install
+if errorlevel 1 exit 1
+
+nmake /f makefile_mod.vc install
 if errorlevel 1 exit 1
 
