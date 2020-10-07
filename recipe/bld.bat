a1de3219f74ac554d3c097c828e0c0259bc7a359
Xylar Asay-Davis <xylarstorm@gmail.com>
2020-10-07 14:39:44 +0200

    Reorder Win build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,10 +2,13 @@
 nmake /f makefile.vc
 if errorlevel 1 exit 1
 
-nmake /f makefile_mod.vc
+nmake /f makefile.vc install
 if errorlevel 1 exit 1
 
-nmake /f makefile.vc install
+nmake /f makefile.vc clean
+if errorlevel 1 exit 1
+
+nmake /f makefile_mod.vc
 if errorlevel 1 exit 1
 
 nmake /f makefile_mod.vc install
