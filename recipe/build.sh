df8dada8e30b1edf914d0062aac597f91e870374
akrherz <akrherz@iastate.edu>
2021-09-21 15:15:00 -0500

    try to verify sqlite<3.36 builds

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,8 +3,7 @@
 cp $BUILD_PREFIX/share/gnuconfig/config.* .
 
 export LDFLAGS="-L${PREFIX}/lib ${LDFLAGS}"
-# workaround #71 sqlite 3.36 issue
-export CFLAGS="-I${PREFIX}/include -DSQLITE_ALLOW_ROWID_IN_VIEW ${CFLAGS}"
+export CFLAGS="-I${PREFIX}/include ${CFLAGS}"
 
 # these files have hardcoded paths in them.  We don't need .la files anyway, so just remove it.
 # if [ -f ${PREFIX}/${HOST}/lib/libstdc++.la ]; then
